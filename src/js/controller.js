8842540b32dc1821304959b863dbb3df53e7462c
goeasy-io_jack <jack@goeasy.io>
2020-06-22 17:34:18 +0800

    修改

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,109 +1,98 @@
-var chatService = new Service();
+var chatService = new IMService();
 
 //枚举，代表三个页面
 var Pages = {
     //登录页面
     login: 0,
-    //好友列表
-    friendList: 1,
-    //聊天页面
-    chatForm: 2
+    //联系人
+    contacts: 1,
+    //私聊界面
+    privateChat: 2,
+    //群聊界面
+    groupChat: 3
 };
-
 
 //标记所处的页面和当前聊天的好友
 var currentPage = {
     page: Pages.login,
-    currentChatFriendUUID: ''
+    currentUser: null,
+    currentChatFriend: null,
+    currentChatGroup: null
 };
 
+//为了避免controller层侵入到service层，用controller层的代码覆盖service层的预置方法
+chatService.onNewPrivateMessageReceive = onNewPrivateMessageReceive;
+chatService.onPrivateHistoryLoad = onPrivateHistoryLoad;
+chatService.onNewGroupMessageReceive = onNewGroupMessageReceive;
+chatService.onGroupHistoryLoad = onGroupHistoryLoad;
+chatService.onFriendListChange = onFriendListChange;
+chatService.onGroupListChange = onGroupListChange;
 
+//登录
 function login() {
     var username = $("#username").val();
     var password = $("#password").val();
-    var result = chatService.login(username, password);
-    if (result.success) {
-
-        //为了避免controller层侵入到service层，用controller层的代码覆盖service层的预置方法
-        chatService.onNewChatMessage = onNewChatMessage;
-        chatService.onFriendOnline = onFriendOnline;
-        chatService.onFriendOffline = onFriendOffline;
-        chatService.onShowToast = showToast;
-
+    var resultStatus = chatService.login(username, password);
+    if (resultStatus) {
         //初始化GoEasy和本地好友列表
-        chatService.initialGoEasyAndFriends();
+        chatService.connectIM();
         $("#login-box").hide();
-        switchToUserList();
-        //初始化好友在线状态
-        chatService.initialFriendOnlineStatus();
-        //修改title
-        $("title").text("好友列表");
+        //切换到联系人
+        switchToContacts();
     } else {
         //显示错误信息
         $("#login-error-box").show();
     }
 }
 
-
-function onFriendOnline(uuid) {
-    // 朋友上线头像变彩色
-    $("#avatar_" + uuid).removeClass("friend-avatar-desaturate");
-    //如果是當前聊天的消息
-    //忽略
-    //如果在用戶列表
-    // 刷新用戶列表
-}
-
-function onFriendOffline(uuid) {
-    //好友下线头像变灰色
-    $("#avatar_" + uuid).addClass("friend-avatar-desaturate");
-    //如果是當前聊天的消息
-    // 忽略
-    //如果在用戶列表，紅色數字
-    // 刷新用戶列表
-}
-
-
-function switchToUserList() {
-    //修改页面的title
-    $("title").text("好友列表");
-
+//切换到联系人界面
+function switchToContacts() {
     //设置当前页面为好友列表页面
-    currentPage.page = Pages.friendList;
+    currentPage.page = Pages.contacts;
+    //修改页面的title
+    $("title").text("联系人");
 
-    //清空之前的好友列表信息
-    var friendListDiv = $("#friend-list");
-    friendListDiv.empty();
-
-    //初始胡当前用户的头像和昵称
-    var currentUser = chatService.currentUser;
-    $("#current-user-avatar").attr("src", currentUser.avatar);
-    $("#current-user-name").text(currentUser.username);
+    //初始化当前用户
+    currentPage.currentUser = chatService.currentUser;
+    $("#current-user-avatar").attr("src", currentPage.currentUser.avatar);
+    $("#current-user-name").text(currentPage.currentUser.name);
 
     //隐藏聊天窗口
     $("#chat-box").hide();
+    $("#group-chat-box").hide();
 
+    //获取好友数据
+    var friendsMap = chatService.friends;
+    //绘制好友列表
+    drawFriendList(friendsMap);
     //显示好友列表
     $("#friends-box").show();
 
-    //获取用户的本地好友列表
-    var friendList = chatService.getFriendList();
+    //获取群数据
+    var groupsMap = chatService.groups;
+    //绘制群列表
+    drawGroupList(groupsMap);
+    //显示群列表
+    $("#group-box").show()
+}
 
-    //展示好友到界面上
-    friendList.forEach(function (friend) {
+//绘制好友列表
+function drawFriendList(friends) {
+    var friendListDiv = $("#friend-list");
+    friendListDiv.empty();
+    for (var key in friends) {
         var item = $("#friend-item-template").clone();//好友信息的模版
+        item.remove('#friend-item-template');
+        var friend = friends[key];
 
         //更新好友uuid
         item.attr("id", friend.uuid);
-
         //设置好友头像
         var friendAvatarImg = item.find("img");
         friendAvatarImg.attr("src", friend.avatar);
-        friendAvatarImg.attr("id", "avatar_" + friend.uuid);
-
+        friendAvatarImg.attr("id", "avatar_" + key);
         //设置好友名称
-        item.find(".friend-name").html(friend.username);
-
+        item.find(".friend-name").html(friend.name);
         //更新未读消息数
         var unReadMessage = friend.unReadMessage;
         item.find(".message-count").text(unReadMessage);
@@ -119,68 +108,67 @@ function switchToUserList() {
         }
 
         //添加点击事件，点击好友条目后，进入聊天窗口
-        item.click(function () {
-            showChatForm(friend.uuid, friend.username, friend.avatar);
-        });
+        (function (key) {
+            var friend = friends[key]
+            item.click(function () {
+                switchToPrivateChat(friend);
+            });
+        })(key)
 
         //将一条好友添加到好友的列表中
         friendListDiv.append(item);
 
-
         if (friend.online) {
-            onFriendOnline(friend.uuid);
+            $("#avatar_" + key).removeClass("friend-avatar-desaturate");
         }
-    });
-
-
-}
-
-//接收到新的聊天消息
-function onNewChatMessage(chatMessage, friend) {
-
-    //如果当前窗口是在好友列表页面，只显示未读消息数
-
-
-    if (currentPage.page == Pages.friendList) {
-        var friendItem = $("#" + friend.uuid);
-        friendItem.find(".message-count").text(friend.unReadMessage);
-        friendItem.find(".friend-item-message-badge").show();
-    } else {
-        var messageTemplate;
-        //如果当前窗口不是与收到消息的朋友的聊天窗口，直接显示
-        if (currentPage.currentChatFriendUUID == friend.uuid) {
-
-            var currentUser = chatService.currentUser;
-
-            //如果消息是自己发送的，显示在右边
-            if (chatMessage.senderUUID == currentUser.uuid) {
-                messageTemplate = $("#chat-box-self-message-template").clone();
-                messageTemplate.find("img").attr("src", currentUser.avatar);
-            } else {
-                //如果是好友发送的，显示在左边
-                messageTemplate = $("#chat-box-friend-message-template").clone();
-                messageTemplate.find("img").attr("src", friend.avatar);
-                chatService.clearUnreadMessage(friend.uuid);
-            }
-            //设置消息内容
-            messageTemplate.find(".chat-message").text("" + chatMessage.content);
-
-            //显示消息到页面
-            var chatBoxContent = $("#chat-box-content");
-            chatBoxContent.append(messageTemplate);
-        }
-
-        //将滚动条拉到最下
-        $('#chat-box-content').scrollTop($('#chat-box-content')[0].scrollHeight);
     }
 }
 
-function showChatForm(friendUUID, friendUsername, friendAvatar) {
-    //设置当前页面为聊天页面和聊天的好友
-    currentPage.page = Pages.chatForm;
-    currentPage.currentChatFriendUUID = friendUUID;
+//绘制群列表
+function drawGroupList(groups) {
+    var groupListDiv = $("#group-list");
+    groupListDiv.empty();
+    for (var key in groups) {
+        var item = $("#group-item-template").clone();//群信息的模版
+        item.remove('#group-item-template');
+        var group = groups[key];
+        //设置群id
+        item.attr("id", key);
+        //设置群名称
+        item.find(".group-name").html(group.name);
+        //更新未读消息数
+        var unReadMessage = group.unReadMessage;
+        item.find(".message-count").text(unReadMessage);
 
-    $("title").text("聊天界面");
+        //显示或隐藏未读消息数
+        var messageBadge = item.find(".friend-item-message-badge");
+        if (unReadMessage == 0) {
+            //没有未读消息，隐藏消息数量
+            messageBadge.hide();
+        } else {
+            //有未读消息，展现未读消息量
+            messageBadge.show();
+        }
+
+        //添加点击事件，点击好友条目后，进入聊天窗口
+        (function (key) {
+            var group = groups[key]
+            item.click(function () {
+                switchToGroupChat(group);
+            });
+        })(key)
+
+        //将一条好友添加到好友的列表中
+        groupListDiv.append(item);
+    }
+}
+
+//切换到私聊界面
+function switchToPrivateChat(friend) {
+    //设置当前页面为聊天页面和聊天的好友
+    currentPage.page = Pages.privateChat;
+    currentPage.currentChatFriend = friend;
+    $("title").text("私聊界面");
 
     //先清空之前的聊天内容
     var chatBoxContent = $("#chat-box-content");
@@ -188,56 +176,225 @@ function showChatForm(friendUUID, friendUsername, friendAvatar) {
 
     //隐藏好友列表
     $("#friends-box").hide();
-
     //更新当前聊天的好友名称
-    $(".current-friend-name").text(friendUsername);
+    $(".current-friend-name").text(friend.name);
 
     //显示聊天窗口
     $("#chat-box").show();
 
     $("#sendMessageButton").off('click').on("click", function () {
-        sendChatMessage(friendUUID);
+        sendPrivateChatMessage(friend.uuid);
     });
 
-    //获取聊天记录
-    var chatHistory = chatService.loadChatHistory(friendUUID);
-    var currentUser = chatService.currentUser;
-
-    chatHistory.forEach(function (chatMessage) {
-        var messageTemplate;
-        //判断这条消息是谁发的
-        if (chatMessage.senderUUID == currentUser.uuid) {
-            //自己发送的消息展示在右边
-            messageTemplate = $("#chat-box-self-message-template").clone();
-            //更新头像
-            messageTemplate.find("img").attr("src", currentUser.avatar);
-        } else {
-            //如果该为好友发送的消息展示在左边
-            messageTemplate = $("#chat-box-friend-message-template").clone();
-            messageTemplate.find("img").attr("src", friendAvatar);
-            chatService.clearUnreadMessage(friendUUID);
-        }
-        messageTemplate.find(".chat-message").text(chatMessage.content);
-        //显示一条消息到页面上
-        chatBoxContent.append(messageTemplate);
-    });
-
-    //将滚动条拉到最下
-	$('#chat-box-content').scrollTop($('#chat-box-content')[0].scrollHeight);
+    //绘制聊天消息
+    var messages = chatService.getPrivateMessages(friend.uuid);
+    if (messages.length != 0) {
+        drawPrivateChatMessage(messages)
+    }
 }
 
-// 发送消息
-function sendChatMessage(friendUUID) {
+//切换到群聊界面
+function switchToGroupChat(group) {
+    //设置当前页面为聊天页面和聊天的好友
+    currentPage.page = Pages.groupChat;
+    currentPage.currentChatGroup = group;
+    $("title").text("群聊界面");
+
+    //先清空之前的聊天内容
+    var chatBoxContent = $("#group-chat-box-content");
+    chatBoxContent.empty();
+
+    //隐藏好友列表
+    $("#friends-box").hide();
+    //更新当前聊天的好友名称
+    $(".current-friend-name").text(group.name);
+
+    //显示聊天窗口
+    $("#group-chat-box").show();
+
+    $("#groupSendMessageButton").off('click').on("click", function () {
+        sendGroupChatMessage(group.uuid);
+    });
+
+    //绘制界面聊天消息
+    var messages = chatService.getGroupMessages(group.uuid)
+    drawGroupChatMessage(messages)
+}
+
+//私聊回到联系人
+function privateChatBackToContacts() {
+    chatService.resetFriendUnReadMessage(currentPage.currentChatFriend);
+    switchToContacts()
+}
+
+// 发送私聊消息
+function sendPrivateChatMessage() {
     //获取content并赋值
     var messageInputBox = $("#send-input-box");
 
     var content = messageInputBox.val();
     if (content != '' && content.trim().length > 0) {
         // 发送消息
-        chatService.sendChat(friendUUID, content);
+        chatService.sendPrivateMessage(currentPage.currentChatFriend.uuid, content);
         //发送消息后输入框清空
         messageInputBox.val("");
     }
+}
+
+//加载私聊历史消息
+function loadPrivateHistory() {
+    var messages = chatService.getPrivateMessages(currentPage.currentChatFriend.uuid);
+    let earliestMessageTimeStamp = Date.now();
+    let earliestMessage = messages[0];
+    if (earliestMessage) {
+        earliestMessageTimeStamp = earliestMessage.timestamp;
+    }
+    this.chatService.loadPrivateHistoryMessage(currentPage.currentChatFriend.uuid, earliestMessageTimeStamp)
+}
+
+//监听私聊消息加载
+function onPrivateHistoryLoad(friendId, messages) {
+    if (messages.length == 0) {
+        $('#top').html('已经没有更多的历史消息');
+        return
+    }
+    var chatMessages = chatService.getPrivateMessages(friendId)
+    drawPrivateChatMessage(chatMessages)
+}
+
+//绘制界面私聊消息
+function drawPrivateChatMessage(privateMessages) {
+    var chatBoxContent = $("#chat-box-content");
+    chatBoxContent.empty();
+    privateMessages.forEach(function (message) {
+        var messageTemplate;
+        //判断这条消息是谁发的
+        if (message.senderId === chatService.currentUser.uuid) {
+            //自己发送的消息展示在右边
+            messageTemplate = $("#chat-box-self-message-template").clone();
+            messageTemplate.remove('#chat-box-self-message-template');
+            //更新头像
+            messageTemplate.find("img").attr("src", chatService.currentUser.avatar);
+        } else {
+            //如果该为好友发送的消息展示在左边
+            messageTemplate = $("#chat-box-friend-message-template").clone();
+            messageTemplate.find("img").attr("src", currentPage.currentChatFriend.avatar);
+        }
+        messageTemplate.find(".chat-message").text(message.payload);
+        //显示一条消息到页面上
+        chatBoxContent.append(messageTemplate);
+    });
+
+    //将滚动条拉到最下
+    $('#chat-box-content').scrollTop($('#chat-box-content').scrollHeight);
+}
+
+//群聊回到联系人
+function groupChatBackToContacts() {
+    chatService.resetGroupUnReadMessage(currentPage.currentChatGroup);
+    switchToContacts()
+}
+
+//发送群聊消息
+function sendGroupChatMessage() {
+    //获取content并赋值
+    var messageInputBox = $("#group-send-input-box");
+
+    var content = messageInputBox.val();
+    if (content != '' && content.trim().length > 0) {
+        // 发送消息
+        chatService.sendGroupMessage(currentPage.currentChatGroup.uuid, content);
+        //发送消息后输入框清空
+        messageInputBox.val("");
+    }
+}
+
+//加载群聊历史消息
+function loadGroupHistory() {
+    var messages = chatService.getGroupMessages(currentPage.currentChatGroup.uuid);
+    let earliestMessageTimeStamp = Date.now();
+    let earliestMessage = messages[0];
+    if (earliestMessage) {
+        earliestMessageTimeStamp = earliestMessage.timestamp;
+    }
+    this.chatService.loadGroupHistoryMessage(currentPage.currentChatGroup.uuid, earliestMessageTimeStamp)
+}
+
+//监听群聊历史消息加载
+function onGroupHistoryLoad(groupId, messages) {
+    if (messages.length == 0) {
+        //todo 文字变化
+        return
+    }
+    ;
+    var chatMessage = chatService.getGroupMessages(groupId)
+    drawGroupChatMessage(chatMessage)
+}
+
+//绘制群聊界面消息
+function drawGroupChatMessage(groupMessages) {
+    var currentUser = chatService.currentUser;
+    var chatBoxContent = $("#group-chat-box-content");
+    chatBoxContent.empty();
+    groupMessages.forEach(function (message) {
+        var messageTemplate;
+        //判断这条消息是谁发的
+        if (message.senderId === currentUser.uuid) {
+            //自己发送的消息展示在右边
+            messageTemplate = $("#chat-box-self-message-template").clone();
+            messageTemplate.remove('#chat-box-self-message-template');
+            //更新头像
+            messageTemplate.find("img").attr("src", currentUser.avatar);
+        } else {
+            //如果该为好友发送的消息展示在左边
+            messageTemplate = $("#chat-box-friend-message-template").clone();
+            messageTemplate.remove('#chat-box-self-message-template');
+
+            var friend = chatService.friends[message.senderId]
+            messageTemplate.find("img").attr("src", friend.avatar);
+        }
+        messageTemplate.find(".chat-message").text(message.payload);
+        //显示一条消息到页面上
+        chatBoxContent.append(messageTemplate);
+    });
+
+    //将滚动条拉到最下
+    $('#chat-box-content').scrollTop($('#group-chat-box-content')[0].scrollHeight);
+}
+
+//监听接收私聊消息
+function onNewPrivateMessageReceive(friendId, chatMessage) {
+    let friend = chatService.friends[friendId];
+    //如果当前窗口是在好友列表页面，只显示未读消息数
+    if (currentPage.page == Pages.contacts) {
+        drawFriendList(chatService.friends)
+    } else {
+        drawPrivateChatMessage(chatService.getPrivateMessages(friendId))
+    }
+}
+
+//监听接收群聊消息
+function onNewGroupMessageReceive(groupId, chatMessage) {
+    let group = chatService.groups[groupId];
+
+    //如果当前窗口是在好友列表页面，只显示未读消息数
+    if (currentPage.page == Pages.contacts) {
+        var groupItem = $("#" + groupId);
+        groupItem.find(".message-count").text(group.unReadMessage);
+        groupItem.find(".friend-item-message-badge").show();
+    } else {
+        drawGroupChatMessage(chatService.getGroupMessages(groupId))
+    }
+}
+
+//更新好友列表
+function onFriendListChange(friends) {
+    drawFriendList(friends)
+}
+
+//更新群列表
+function onGroupListChange(groups) {
+    drawGroupList(groups)
 }
 
 //全局计时器
@@ -261,3 +418,4 @@ function showToast(message) {
 function hideToast() {
     $('.toast').hide().empty();
 }
+
---
+++ src/js/imservice.js
@@ -0,0 +1,336 @@
+/*
+ * @Author: jack.lu
+ * @Date: 2020-4-21 10:10:20
+ * @Last Modified by: jack.lu
+ * @Last Modified time: 2020-4-21 15:01:41
+ */
+
+function Friend(uuid, name, avatar) {
+    this.uuid = uuid;
+    this.name = name;
+    this.avatar = avatar;
+    this.online = false;
+    this.unReadMessage = 0;
+}
+
+function Group(uuid, name, avatar) {
+    this.uuid = uuid;
+    this.name = name;
+    this.avatar = avatar;
+    this.unReadMessage = 0;
+}
+
+function CurrentUser(uuid, name, avatar) {
+    this.uuid = uuid;
+    this.name = name;
+    this.avatar = avatar;
+}
+
+function IMService() {
+    this.im = GoEasyIM.getInstance({
+        appkey: '',
+        host: 'testhangzhou.goeasy.io',
+    });
+    //当前“我”
+    this.currentUser = null;
+    //我的好友
+    this.friends = {};
+    //我的群
+    this.groups = {};
+    //私聊消息记录，map格式，每个好友对应一个数组
+    this.privateMessages = {};
+    //群聊消息记录，map格式，每个群对应一个数组
+    this.groupMessages = {};
+    /*
+     * 监听器们
+     *
+     * 可以在页面里，根据需求，重写以下监听器，
+     * 便于当各种事件触发时，页面能够执行对应的响应
+     *
+     */
+    //收到一条私聊消息
+    this.onNewPrivateMessageReceive = function (friendId, message) {
+    };
+    //完成一次私聊历史加载
+    this.onPrivateHistoryLoad = function (friendId, messages) {
+    };
+    //收到一条群聊消息
+    this.onNewGroupMessageReceive = function (groupId, message) {
+    };
+    //完成一次群聊历史加载
+    this.onGroupHistoryLoad = function (groupId, messages) {
+    };
+    //好友列表发生改变
+    this.onFriendListChange = function (friends) {
+    };
+    //群列表发生改变
+    this.onGroupListChange = function (groups) {
+    };
+}
+
+//登录
+IMService.prototype.login = function (username, password) {
+    var user = restApi.findUser(username, password);
+    if (user) {
+        //初始化当前用户
+        this.currentUser = new CurrentUser(user.uuid, user.name, user.avatar);
+        //初始化联系人信息，包括群
+        this.initialContacts();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+//初始化联系人信息
+IMService.prototype.initialContacts = function () {
+    //查询并初始化好友信息
+    let friendList = restApi.findFriends(this.currentUser);
+
+    //将用户列表初始化为一个map，便于后续根据friendId得到friend
+    friendList.map(friend => {
+        this.friends[friend.uuid] = new Friend(friend.uuid, friend.name, friend.avatar);
+    });
+
+    //查询并初始化与自己相关的群信息
+    let groupList = restApi.findGroups(this.currentUser);
+
+    //将群列表初始化为一个map，方便后续根据groupId索引
+    groupList.map(group => {
+        this.groups[group.uuid] = new Group(group.uuid, group.name, group.avatar);
+    });
+};
+
+//获取群成员
+IMService.prototype.getGroupMembers = function (groupId) {
+    let members = restApi.findGroupMembers(groupId);
+    let membersMap = {};
+    members.map(item => {
+        membersMap[item.uuid] = item
+    });
+    return membersMap;
+};
+
+IMService.prototype.getGroupMessages = function (groupId) {
+    if (!this.groupMessages[groupId]) {
+        this.groupMessages[groupId] = [];
+    }
+    return this.groupMessages[groupId]
+};
+
+IMService.prototype.getPrivateMessages = function (friendId) {
+    if (!this.privateMessages[friendId]) {
+        this.privateMessages[friendId] = [];
+    }
+    return this.privateMessages[friendId]
+};
+
+//重置群聊未读消息
+IMService.prototype.resetGroupUnReadMessage = function (group) {
+    this.groups[group.uuid].unReadMessage = 0;
+    this.onGroupListChange(this.groups);
+};
+
+//将好友的未读消息数字清零
+IMService.prototype.resetFriendUnReadMessage = function (friend) {
+    this.friends[friend.uuid].unReadMessage = 0;
+    this.onFriendListChange(this.friends);
+};
+
+//连接GoEasy
+IMService.prototype.connectIM = function () {
+    //初始化IM相关的监听器
+    this.initialIMListeners();
+    this.im.connect({
+        id: this.currentUser.uuid,
+        data: {
+            avatar: this.currentUser.avatr,
+            name: this.currentUser.name
+        }
+    }).then(() => {
+        console.log('连接成功')
+        //订阅与自己相关的群信息
+        this.subscribeGroupMessage();
+        //初始化好友们的在线状态
+        this.initialFriendOnlineStatus();
+        //订阅我的好友们的上下线信息
+        this.subscribeFriendsPresence();
+    }).catch(error => {
+        console.log('连接失败,请确保网络正常，appkey和host正确，code:' + error.code + " content:" + error.content);
+    });
+};
+
+//IM监听
+IMService.prototype.initialIMListeners = function () {
+    this.im.on(GoEasyIM.EVENT.CONNECTED, () => {
+        console.log('连接成功.')
+    });
+
+    this.im.on(GoEasyIM.EVENT.DISCONNECTED, () => {
+        console.log('连接断开.')
+    });
+
+    //监听好友上下线
+    this.im.on(GoEasyIM.EVENT.USER_PRESENCE, (user) => {
+        //更新好友在线状态
+        let onlineStatus = user.action == 'online' ? true : false;
+        let friend = this.friends[user.userId];
+        friend.online = onlineStatus;
+
+        //如果页面传入了相应的listener，执行listener
+        this.onFriendListChange(this.friends);
+    });
+
+    //监听私聊消息
+    this.im.on(GoEasyIM.EVENT.PRIVATE_MESSAGE_RECEIVED, (message) => {
+        //如果不是自己发的，朋友未读消息数 +1
+        if (message.senderId != this.currentUser.uuid) {
+            let friend = this.friends[message.senderId];
+            friend.unReadMessage++;
+            this.onFriendListChange(this.friends);
+        }
+
+        //更新私聊消息记录
+        let friendId;
+        if (this.currentUser.uuid == message.senderId) {
+            friendId = message.receiverId;
+        } else {
+            friendId = message.senderId;
+        }
+
+        let friendMessages = this.getPrivateMessages(friendId);
+        friendMessages.push(message);
+
+        //如果页面传入了相应的listener，执行listener
+        this.onNewPrivateMessageReceive(friendId, message);
+    });
+
+    //监听群聊消息
+    this.im.on(GoEasyIM.EVENT.GROUP_MESSAGE_RECEIVED, (message) => {
+        //群未读消息+1
+        let groupId = message.groupId;
+        let group = this.groups[groupId];
+        group.unReadMessage++;
+
+        //如果页面传入了相应的listener，执行listener
+        this.onGroupListChange(this.groups);
+
+        //更新群聊消息记录
+        let groupMessages = this.getGroupMessages(groupId);
+        groupMessages.push(message);
+
+        //如果页面传入了相应的listener，执行listener
+        this.onNewGroupMessageReceive(groupId, message);
+    })
+};
+
+//订阅群消息
+IMService.prototype.subscribeGroupMessage = function () {
+    let groupIds = Object.keys(this.groups);
+    this.im.subscribeGroup(groupIds)
+        .then(() => {
+            console.log('订阅群消息成功')
+        })
+        .catch(error => {
+            console.log('订阅群消息失败')
+            console.log(error)
+        })
+};
+
+//初始化好友在线状态
+IMService.prototype.initialFriendOnlineStatus = function () {
+    let friendIds = Object.keys(this.friends);
+    this.im.hereNow({
+        userIds: friendIds
+    }).then(result => {
+        let onlineFriends = result.content;
+        onlineFriends.map(user => {
+            let friend = this.friends[user.userId];
+            friend.online = true;
+        });
+        this.onFriendListChange(this.friends);
+    }).catch(error => {
+        console.log(error)
+        if (error.code == 401) {
+            console.log("获取在线用户失败，您尚未开通用户在线状态，请登录GoEasy，查看应用详情里自助启用.");
+        }
+    })
+};
+
+//监听所有好友上下线
+IMService.prototype.subscribeFriendsPresence = function () {
+    let friendIds = Object.keys(this.friends);
+    this.im.subscribeUserPresence(friendIds)
+        .then(() => {
+            console.log('监听好友上下线成功')
+        })
+        .catch(error => {
+            console.log(error);
+            if (error.code == 401) {
+                console.log("监听好友上下线失败，您尚未开通用户状态提醒，请登录GoEasy，查看应用详情里自助启用.");
+            }
+        });
+};
+
+//加载单聊历史消息
+IMService.prototype.loadPrivateHistoryMessage = function (friendId, timeStamp) {
+    this.im.history({
+        friendId: friendId,
+        lastTimestamp: timeStamp
+    }).then(result => {
+        let history = result.content;
+        let friendMessages = this.getPrivateMessages(friendId);
+        for (let i = history.length - 1; i >= 0; i--) {
+            friendMessages.unshift(history[i])
+        }
+        //如果页面传入了相应的listener，执行listener
+        this.onPrivateHistoryLoad(friendId, history);
+    }).catch(error => {
+        console.log(error);
+        if (error.code == 401) {
+            console.log("您尚未开通历史消息，请登录GoEasy，查看应用详情里自助启用.");
+        }
+    })
+};
+
+//发送私聊消息
+IMService.prototype.sendPrivateMessage = function (friendId, message) {
+    let chatMessage = this.im.createMessage(message);
+    this.im.sendPrivateMessage(friendId, chatMessage)
+        .then(() => {
+            console.log('发送私聊成功')
+        }).catch(error => {
+        console.log(error)
+    })
+};
+
+//发送群聊消息
+IMService.prototype.sendGroupMessage = function (groupId, message) {
+    let chatMessage = this.im.createMessage(message);
+    this.im.sendGroupMessage(groupId, chatMessage)
+        .then(() => {
+            console.log('发送群聊成功')
+        }).catch(error => {
+        console.log(error)
+    })
+};
+
+//群聊历史消息
+IMService.prototype.loadGroupHistoryMessage = function (groupId, timeStamp) {
+    this.im.history({
+        groupId: groupId,
+        lastTimestamp: timeStamp
+    }).then(result => {
+        let history = result.content;
+        let groupMessages = this.getGroupMessages(groupId);
+        for (let i = history.length - 1; i >= 0; i--) {
+            groupMessages.unshift(history[i]);
+        }
+        this.onGroupHistoryLoad(groupId, history);
+    }).catch(error => {
+        console.log(error)
+        if (error.code == 401) {
+            console.log("您尚未开通历史消息，请登录GoEasy，查看应用详情里自助启用.");
+        }
+    })
+};
